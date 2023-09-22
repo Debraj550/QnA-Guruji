ae6e27b866e981af7d5fde793c26511c22bef2e7
Debraj Dhar <debrajdhar100@gmail.com>
2023-09-22 13:48:04 +0530

    Deploying the project on git

---
+++ src/components/Body.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import * as qna from "@tensorflow-models/qna";
+import * as tf from "@tensorflow/tfjs";
+import "../components/body.css";
+import predata from "../utils/predata";
+
+const Body = () => {
+  const [model, setModel] = useState(null);
+  let [color, setColor] = useState("#ffffff");
+  const [question, setQuestion] = useState("");
+  const [passage, setPassage] = useState(predata);
+  const [answers, setAnswers] = useState([]);
+  const [err, setErr] = useState(0);
+
+  useEffect(() => {
+    async function loadQnAModel() {
+      await tf.setBackend("webgl");
+      const qnaModel = await qna.load();
+      setModel(qnaModel);
+    }
+
+    loadQnAModel();
+  }, []);
+
+  const handleQuestionChange = (event) => {
+    setQuestion(event.target.value);
+  };
+
+  const handlePassageChange = (event) => {
+    setPassage(event.target.value);
+  };
+
+  const findAnswers = async () => {
+    try {
+      if (!model || !question || !passage) {
+        setErr(1);
+        setAnswers([]);
+        return;
+      }
+      setErr(0);
+      const results = await model.findAnswers(question, passage);
+      console.log(results);
+      setAnswers(results);
+    } catch (error) {
+      console.error("Error finding answers:", error);
+    }
+  };
+
+  return (
+    <div className="body-container">
+      {model ? (
+        <div>
+          <h1>QnA-Guruji</h1>
+
+          <div className="passage-container">
+            <label>Passage:</label>
+            <textarea
+              value={passage}
+              onChange={handlePassageChange}
+              style={{ width: "500px", height: "200px" }}
+            />
+          </div>
+          <div className="question-container">
+            <label>Question:</label>
+            <input
+              type="text"
+              value={question}
+              onChange={handleQuestionChange}
+              style={{ width: "400px", height: "50px" }}
+            />
+          </div>
+          <button className="find-answer-btn" onClick={findAnswers}>
+            Find Answers
+          </button>
+          <div className="answer-container">
+            <h3>Answers:</h3>
+            {err ? (
+              <div style={{ color: "red" }}>
+                {"Error: Kindly provide the missing fields."}
+              </div>
+            ) : (
+              ""
+            )}
+            <ul>
+              {answers.map((answer, index) => (
+                <li key={index}>{answer.text}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Model is still loading.</h1>
+          <ClipLoader
+            color={color}
+            loading={!model}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/utils/predata.js
@@ -0,0 +1,39 @@
+const predata = `{
+   "Scholarships": [
+      {
+         "Scholarship Name": "Post Matric Scholarship Scheme",
+         "Details": {
+            "Website": "https://dbttribal.gov.in/",
+            "Description": "A Centrally Sponsored Scheme implemented through States/UTs for providing scholarships to eligible Scheduled Tribe (ST) students pursuing recognized courses in recognized institutions.",
+            "Implementation": "Implemented by States/UTs who are responsible for inviting applications, eligibility verification, and disbursement of scholarships to eligible ST students directly to their bank accounts through Direct Benefit Transfer (DBT).",
+            "Funding": {
+               "Source": "Ministry",
+               "Funds Release": "Funds are released by the Ministry to the State Governments/UTs based on their proposal, including a Statement of Expenditure, Utilization Certificate, and beneficiary data uploaded on the DBT Portal.",
+               "Funds Sharing": "Funds are shared at different ratios between the Centre and State Governments/UTs, with special provisions for Northeastern and Special Category States/UTs. UTs without a legislature receive 100% grant from the Centre."
+            },
+            "Eligibility": {
+               "Course Requirement": "Applicable to students pursuing any recognized course from a recognized institution with a qualification level of Matriculation/Class X or above.",
+               "Income Limit": "Parental income from all sources should not exceed Rs. 2.50 lakhs per annum."
+            },
+            "Scholarship Components": {
+               "Component 1": "Payment of compulsory fees charged by educational institutions, subject to the limit fixed by the concerned State.",
+               "Component 2": "Payment of Maintenance amount, which varies from Rs. 230 to Rs. 1200 per month, depending on the course of study pursued by the student."
+            },
+            "Funds Ratio": {
+               "Centre vs. State/UT": "75:25 for most States/UTs, 90:10 for Northeastern and Special Category States/UTs.",
+               "UT without legislature": "100% grant provided by the Centre."
+            }
+         }
+      },
+      {
+         "Scholarship Name": "Pre Matric Scholarship Scheme",
+         "Details": {
+            "Website": "https://dbttribal.gov.in/",
+            "Description": "A Centrally Sponsored Scheme implemented through States/UTs to provide scholarships to eligible Scheduled Tribe (ST) students in Classes IX – X.",
+            "Implementation": "States/UTs are responsible for inviting applications, eligibility verification, and disbursement of scholarships to eligible ST students directly to their bank accounts through DBT.",
+            "Funding": {
+               "Source": "Ministry",
+               "Funds
+`;
+
+export default predata;
